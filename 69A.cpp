03ef77332aa67d260687cd42bde7c19553278d5a
Levan Kankadze (ლევან კანკაძე) <35868878+Isenmouthe@users.noreply.github.com>
2020-01-13 11:00:59 +0100

    Update 69A.cpp

--- 69A.cpp
+++ 69A.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-  int n, x=0, y=0, z=0;
-  cin>>n;
-  for(int i=0; i<n; i++){
-    int x1, x2, x3;
-    cin>>x1>>x2>>x3;
-    x+=x1; y+=x2; z+=x3;
-}
-if(x==0 && y==0 && z==0)
-  cout<<"YES"<<endl;
- else
-   cout<<"NO"<<endl;
-return 0;
-}
+    #include <iostream> 
+     
+    using namespace std;
+     
+    int main(){
+        int n, x, y, z, x0 = 0, y0 = 0, z0 = 0;
+        cin >> n;
+        for( int i = 0; i < n; i++){
+            cin >> x >> y >> z;
+            x0 = x0 + x;
+            y0 = y0 + y;
+            z0 = z0 + z;
+        }
+        if( x0 == 0 && y0 == 0 && z0 == 0)
+            cout << "YES";
+        else
+            cout << "NO";
+        return 0;
+    }
